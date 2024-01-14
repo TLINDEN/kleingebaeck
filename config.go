348e17c1a2056061bc4b4fe1cc5644378e478383
Thomas von Dein <tom@vondein.org>
2024-01-14 12:15:09 +0100

    fix #30: revert default adnamedir to just use the slug as before

--- config.go
+++ config.go
@@ -43,7 +43,7 @@ const (
 		"Category: {{.Category}}\r\nCondition: {{.Condition}}\r\nCreated: {{.Created}}\r\n\r\n{{.Text}}\r\n"
 	Useragent string = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
 		"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
-	DefaultAdNameTemplate string = "{{.Slug}}-{{.Id}}"
+	DefaultAdNameTemplate string = "{{.Slug}}"
 )
 
 const Usage string = `This is kleingebaeck, the kleinanzeigen.de backup tool.
