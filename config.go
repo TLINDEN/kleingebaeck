dbb64dcae18a72f790d527ff17a2c5c34b19917e
Thomas von Dein <tom@vondein.org>
2024-04-26 12:03:35 +0200

    bump version

--- config.go
+++ config.go
@@ -34,7 +34,7 @@ import (
 )
 
 const (
-	VERSION    string = "0.3.5"
+	VERSION    string = "0.3.6"
 	Baseuri    string = "https://www.kleinanzeigen.de"
 	Listuri    string = "/s-bestandsliste.html"
 	Defaultdir string = "."
