e18577b25a1d1e055858da4ea9ec8687e21b2a2e
Thomas von Dein <tom@vondein.org>
2025-02-06 10:34:23 +0100

    break if match

--- scrape.go
+++ scrape.go
@@ -128,6 +128,7 @@ func ScrapeAd(fetch *Fetcher, uri string) error {
 	for _, detail := range advertisement.Details {
 		if slices.Contains(CONDITIONS, detail) {
 			advertisement.Condition = detail
+			break
 		}
 
 	}
